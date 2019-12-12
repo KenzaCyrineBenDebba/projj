4275dad95581664fdfb04907cce08637406b3cc2
kenzaonly <47718528+kenzaonly@users.noreply.github.com>
2019-12-12 22:51:44 +0100

    Create index 2.php

---
+++ php-crud/entities/crud avis/index 2.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+	<title> PHP CRUD </title>
+	<script src="https://code.jquery.com/jquery-2.1.3.min.js"> </script>
+	<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5"></script> -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> 
+<!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> -->
+
+</head>
+	<body>
+	
+	<?php require_once 'process 2.php'; ?> 
+	<?php 
+	
+	if(isset($_SESSION['message'])): ?>
+	
+<div class="alert alert-<?=$_SESSION['msg_type']?>">
+	
+	<?php
+	echo $_SESSION['message'];
+	unset($_SESSION['message']);
+	?>
+</div>
+
+<?php endif ?>
+	<div class="container">
+	<?php
+	  $mysqli= new mysqli('localhost', 'root', '' ,'crud') or die (mysqli_error($mysqli));
+	  $result =$mysqli->query("SELECT * FROM avis") or die($mysqli->error);
+	  //pre-r($result);
+	  //pre-r($result->fetch_assoc());
+	  //pre-r($result->fetch_assoc());
+	  ?>
+	 
+	  <?php 
+  
+	  
+	  function pre_r($array) {
+		  echo '<pre>';
+		  print_r($array);
+		  echo '</pre>';
+	  }
+	  
+	?>
+	<div class="row justify-content-center">
+		<form action="process 2.php" method="POST" id="myTable">
+		<input type="hidden" name="Id" value="<?php echo $id; ?>">
+		
+		<div class= "form-group">
+		<label> Name </label><span class="error">* <?php echo $nameErr;?></span>
+		<input type="text" name="Name" class="form-control" value="<?php echo $name;  ?>" placeholder="enter your name" required>
+		
+		</div>
+		<div class="form-group">
+		<label> Email </label><span class="error">* <?php echo $emailErr;?></span>
+		<input type="email" name="Email" class="form-control" value="<?php echo $email; ?>" placeholder="enter your email" required>
+		
+		</div>
+		<div class="form-group">
+		<label> Feedback </label>
+		<input type="text" name="feedback" class="form-control"  value="<?php echo $feedback; ?>" placeholder="enter your feedback" required>
+		
+		</div>
+		<div class="form-group">
+		<?php
+		if($Update == true):
+		?>
+		<button type="submit" class="btn btn-info" name="Update"> Update</button>
+		<?php else: ?>
+		<button type="submit" class="btn btn-primary" name="save"> Save</button>
+		<?php endif; ?>
+		</div>
+		</form>
+		</div>
+		</div>
+	</body>
+
+</html>
